022480e0aea54c3cd727d713de4c2dc2037f6aeb
Giselle <gisellewang17@gmail.com>
2022-07-09 13:13:15 -0400

    edit UserMain

--- src/InsertUser.java
+++ src/InsertUser.java
@@ -4,14 +4,18 @@ public class InsertUser{
     private static final String CONNECTION = "jdbc:mysql://localhost:3306/mybnb"; 
 
     private String UserID;
+    private String first_name;
+    private String last_name;
     private String address;
     private String date_of_birth;
     private String occupation;
     private int SIN;
     private boolean success;
 
-    public InsertUser(String UserID, String address, String date_of_birth, String occupation, int SIN){
+    public InsertUser(String UserID, String first_name, String last_name, String address, String date_of_birth, String occupation, int SIN){
         this.UserID = UserID;
+        this.first_name = first_name;
+        this.last_name = last_name;
         this.address = address;
         this.date_of_birth = date_of_birth;
         this.occupation = occupation;
@@ -23,17 +27,19 @@ public class InsertUser{
         final String PASS = "giselle";
         try{
             Connection con = null;
-            String sql = "INSERT INTO Users (UserID, address, date_of_birth, occupation, SIN) VALUES (?,?,?,?,?)";
+            String sql = "INSERT INTO Users (UserID, first_name, last_name, address, date_of_birth, occupation, SIN) VALUES (?,?,?,?,?,?,?)";
             
             con = DriverManager.getConnection(CONNECTION,USER,PASS);
             PreparedStatement ps = con.prepareStatement(sql);
 
             con.setAutoCommit(false);
             ps.setString(1, this.UserID);
-            ps.setString(2, this.address);
-            ps.setString(3, this.date_of_birth);
-            ps.setString(4, this.occupation);
-            ps.setInt(5, this.SIN);
+            ps.setString(2, this.first_name);
+            ps.setString(3, this.last_name);
+            ps.setString(4, this.address);
+            ps.setString(5, this.date_of_birth);
+            ps.setString(6, this.occupation);
+            ps.setInt(7, this.SIN);
 
             success = ps.execute();
 
