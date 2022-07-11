a60f9dfa1facfe9c0bd262bac337172cfc46d35b
Giselle <gisellewang17@gmail.com>
2022-07-10 20:59:09 -0400

    link listing, user, payment in Main

---
+++ src/AvailabilityInput.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class AvailabilityInput {
+    public void getAvailabilityInfo() throws ClassNotFoundException
+    {
+        Scanner inputLID = new Scanner(System.in);
+        System.out.print("Enter the Listing ID: ");
+        int LID = inputLID.nextInt();
+
+        Scanner inputdate= new Scanner(System.in);
+        System.out.print("Enter the date in the form dd/mm/yyyy: ");
+        String date = inputdate.nextLine();
+
+        Scanner inputavailability= new Scanner(System.in);
+        System.out.print("Enter the availability (available/not available): ");
+        String availability  = inputavailability.nextLine();
+
+        Scanner inputprice = new Scanner(System.in);
+        System.out.print("Enter the price: ");
+        float price = inputprice.nextFloat();
+
+
+        InsertAvailability myAvailability = new InsertAvailability(LID, date, availability, price);
+        boolean result = myAvailability.createAvailability();
+
+        inputLID.close();
+        inputdate.close();
+        inputavailability.close();
+        inputprice.close();
+
+        if(!result){
+            System.out.println("Availability was successfully added!");
+        }else{
+            return;
+        }
+
+    }
+}
